5e5ace70aa0d3ac2032251440c5f0386376d799a
Lucas Floriani <lucas.floriane@gmail.com>
2018-05-20 19:57:30 -0300

    Function multiple returns example

--- cards/deck.go
+++ cards/deck.go
@@ -34,3 +34,9 @@ func (d deck) print() {
 		fmt.Println(i, card)
 	}
 }
+
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
+}
+
+// fruits[startIndexIncluding:upToNotIncluding]
