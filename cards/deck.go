bb70e5c43752be51483b1941e0f173bdb6388bac
Lucas Floriani <lucas.floriane@gmail.com>
2018-05-20 19:35:12 -0300

    Add new method function named newDeck to card 'object'

--- cards/deck.go
+++ cards/deck.go
@@ -8,6 +8,21 @@ import "fmt"
 // []string
 type deck []string
 
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
+}
+
 // Any variable of type "deck"
 // now gets access to the
 // "print" method
