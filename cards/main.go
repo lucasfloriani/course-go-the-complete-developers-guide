df62335fe8155197d36aa65d3c6baca37db37ced
Lucas Floriani <lucas.floriane@gmail.com>
2018-05-20 13:31:05 -0300

    Project cards: example to declarate functions with returns

--- cards/main.go
+++ cards/main.go
@@ -3,9 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	// var card string = "Ace of Spades"
-	card := "Ace of Spades"
-	card = "Five of Diamonds" // Dont use column to reasign because the var already know it is for string values
+	// // var card string = "Ace of Spades"
+	// card := "Ace of Spades"
+	// card = "Five of Diamonds" // Dont use column to reasign because the var already know it is for string values
+
+	card := newCard()
 
 	fmt.Println(card)
+}
+
+func newCard() /*int*/ string {
+	return "Five of Diamonds"
+	// return 12
 }
