f61d09925c65232f6f49764701a3e655c0a2caa6
Lucas Floriani <lucas.floriane@gmail.com>
2018-05-21 19:42:27 -0300

    Refactor shuffle method to use a time seed

--- cards/deck.go
+++ cards/deck.go
@@ -6,6 +6,7 @@ import (
 	"math/rand"
 	"os"
 	"strings"
+	"time"
 )
 
 // Create a new type of 'deck'
@@ -67,9 +68,16 @@ func newDeckFromFile(filename string) deck {
 	return deck(s)
 }
 
+// rand.Intn() executes with the same seed everytime
+// it results in the same order when executing
+//
+// Improved version using a time seed
 func (d deck) shuffle() {
+	source := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(source)
+
 	for i := range d {
-		newPosition := rand.Intn(len(d) - 1)
+		newPosition := r.Intn(len(d) - 1)
 		d[i], d[newPosition] = d[newPosition], d[i]
 	}
 }
