0d0be8775f6f1e4f037c8f5330435b091555c6e7
Lucas Floriani <lucas.floriane@gmail.com>
2018-05-23 21:46:09 -0300

    Alternative way to declare embed structs and pointers receiver problem example

--- structs/main.go
+++ structs/main.go
@@ -7,22 +7,31 @@ type contactInfo struct {
 	zipCode int
 }
 
-// Custom type in a custom type
 type person struct {
-	firstName string
-	lastName  string
-	contact   contactInfo
+	firstName   string
+	lastName    string
+	contactInfo // Same as "contactInfo contactInfo"
 }
 
 func main() {
 	jim := person{
 		firstName: "Jim",
 		lastName:  "Party",
-		contact: contactInfo{
+		contactInfo: contactInfo{
 			email:   "jim@gmail.com",
 			zipCode: 94000,
 		},
 	}
 
-	fmt.Printf("%+v\n", jim)
+	jim.print()
+	jim.updateName("Jimmy") // Dont change the name of type person
+	jim.print()
+}
+
+func (p person) print() {
+	fmt.Printf("%+v\n", p)
+}
+
+func (p person) updateName(newFirstName string) {
+	p.firstName = newFirstName
 }
