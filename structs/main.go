bd0d08132bc32e05ec0b1312013cfa4ffc8a42d6
Lucas Floriani <lucas.floriane@gmail.com>
2018-05-23 21:19:42 -0300

    Ways to create struct variable

--- structs/main.go
+++ structs/main.go
@@ -1,10 +1,22 @@
 package main
 
+import "fmt"
+
 type person struct {
 	firstName string
 	lastName  string
 }
 
 func main() {
+	// First way to create a variable of type person
+	alex := person{"Alex", "Anderson"}
+	fmt.Println(alex)
+	fmt.Println(alex.firstName)
+	fmt.Println(alex.lastName)
 
+	// Second way to create a variable of type person
+	lucas := person{firstName: "Lucas", lastName: "Floriani"}
+	fmt.Println(lucas)
+	fmt.Println(lucas.firstName)
+	fmt.Println(lucas.lastName)
 }
