26a99fa3d61870b6f07e37f47ad6f08f90814673
Lucas Floriani <lucas.floriane@gmail.com>
2018-05-23 21:27:31 -0300

    Third way to declare a struct variable and how to assign new values in struct

--- structs/main.go
+++ structs/main.go
@@ -8,6 +8,16 @@ type person struct {
 }
 
 func main() {
+	/*
+	 * == Zero value ==
+	 * When a variable is created without value assign
+	 *
+	 * string == ""
+	 * int == 0
+	 * float == 0
+	 * bool == false
+	 **/
+
 	// First way to create a variable of type person
 	alex := person{"Alex", "Anderson"}
 	fmt.Println(alex)
@@ -19,4 +29,16 @@ func main() {
 	fmt.Println(lucas)
 	fmt.Println(lucas.firstName)
 	fmt.Println(lucas.lastName)
+
+	// Third way to create a variable of type person
+	var roberto person
+	fmt.Println(roberto)
+	fmt.Println(roberto.firstName)
+	fmt.Println(roberto.lastName)
+	// %+v will print all the diferent fields
+	// names and their values from roberto
+	fmt.Printf("%+v\n", roberto)
+	roberto.firstName = "Roberto"
+	roberto.lastName = "Rural"
+	fmt.Printf("%+v", roberto)
 }
