1234b28451cf2b57e860450e20659642519ac2b9
Lucas Floriani <lucas.floriane@gmail.com>
2018-05-31 23:26:53 -0300

    Finish program thats checks if website is up (without channels)

--- channels/main.go
+++ channels/main.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
 func main() {
 	links := []string{
 		"http://google.com",
@@ -7,9 +12,20 @@ func main() {
 		"http://stackoverflow.com",
 		"http://golang.org",
 		"http://amazon.com",
+		"http://askhdkajsd.com",
 	}
 
 	for _, link := range links {
-
+		checkLink(link)
 	}
+}
+
+func checkLink(link string) {
+	_, err := http.Get(link)
+	if err != nil {
+		fmt.Println(link, "might be down!")
+		return
+	}
+
+	fmt.Println(link, "is up!")
 }
